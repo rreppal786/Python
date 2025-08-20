c2a086c07bfb10d01819a76de6f6b263aff9749e
rreppal786 <rreppal786@gmail.com>
2025-08-20 09:03:56 -0500

    printfunction example committed

---
+++ printfunct.py
@@ -0,0 +1,8 @@
+print("Hi  \"Python is fun!\"")
+print('Hi, \"This is my first python code \"')
+print("path:C:\\Users\\User\\Documents\\Python\\printfunct.py")
+print("Message1\tMessage2")
+print("""your learning python:
+\t-Python Basics
+\t-Data Engineering
+\t-AI and ML""")
