12e803b5d089fab5ea64f9bfbda3c31e372e4ea0
rreppal786 <rreppal786@gmail.com>
2025-08-24 00:48:18 -0500

    nerw datatypes

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+age= 30
+hight=5.3
+name='reppal'
+student=True
+course=None
+print( age)
+print(type(age), "\n\t", type(hight), "\n\t", type(name), "\n\t", type(student), "\n\t", type(course) )
+print(  len(name))
